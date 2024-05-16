08ebfaca70ab1dd669001f79c97ddfef9861651a
AlexandreLupepsaBrito <141967601+AlexandreLupepsaBrito@users.noreply.github.com>
2024-05-15 21:58:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'React';
+import React, {Component} from 'react';
 import './estilo.css';
 
-import biscoito from './assets/biscoito.png';
+  import biscoito from './assets/biscoito.png';
 
-class App extends Component {
+    class App extends Component{
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      textoFrase: ''
-    };
-    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+      constructor(props){
+        super(props);
+        this.state = {
+          textoFrase: ' '
+        };
+        this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
 
-    this.frases = ['Siga os bons e aprenda com eles', 'O bom-senso vale mais do que muito conhecimento', 'O riso é a menor distanica entre duas pessoas', 'Deixe de lado as preocupações e seja feliz', 'Realize o óbvio, pense no improvável e conquiste o Impossível', 'Acredite em milagres, mas não dependa deles', 'A maior barreira para o sucesso é o medo do fracasso'];
-  }
+        this.frase = ['Siga os bons e aprenda com eles', 'O bom-senso vale mais que muito conhecimento','O riso é a menor distancia entre duas pessoas', 'Deixe de lado as preocupações e seja feliz', 'Realieze o óbvio, pense no improvável e conquiste o impossivel', 'Acredite em milagres, mas não dependa deles', 'A maior bvarreira para o sucesso é o medo do fracasso'];
+      }
 
-  quebrarBiscoito(){
-    let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    state.textoFrase = this.frases[numeroAleatorio]
-    this.setState(state);
-  }
+      quebrarBiscoito(){
+        let state = this.state;
+        let numeroAleatorio = Math.floor(Math.random() * this.frase.length)
+        state.textoFrase = this.frase[numeroAleatorio];
+        this.setState(state);
+      }
+      render(){
+        return(
+          <div className='container'>
+            <img src={biscoito} className="img" alt= "imagem do biscoito"/> 
+            <Botao acao={this.quebrarBiscoito} nome="Abrir Biscoito"></Botao>
+            <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+          </div>
+        );
+      }
+    }
 
-}
+    class Botao extends Component{
+      render(){
+        return(
+          <div>
+            <button onClick={this.props.acao}>{this.props.nome}</button>
+          </div>
+        )
+      }
+      
+    }
+
+    export default App;
